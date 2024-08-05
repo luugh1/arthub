4235a2cd20d0d3eba1d284e475cdfc60422750c8
CierraCon <cbaileyrice8948@conestogac.on.ca>
2024-08-05 13:23:47 -0400

    Update - Home Page Sections Completed

--- js/home-script.js
+++ js/home-script.js
@@ -1,34 +1,91 @@
+// Paintings section
 const paintingsContainers = [...document.querySelectorAll('.paintings-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
 
 paintingsContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let containerWidth = containerDimensions.width;
+    const containerWidth = item.offsetWidth;
 
     nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
-    })
+        item.scrollBy({
+            left: containerWidth,
+            behavior: 'smooth'
+        });
+    });
 
     preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-    })
-})
+        item.scrollBy({
+            left: -containerWidth,
+            behavior: 'smooth'
+        });
+    });
+});
 
-
+// Sculptures section
 const sculptContainers = [...document.querySelectorAll('.sculpt-container')];
 const scnxtBtn = [...document.querySelectorAll('.scnxt-btn')];
 const scpreBtn = [...document.querySelectorAll('.scpre-btn')];
 
 sculptContainers.forEach((item, i) => {
-    let containerDimensions = item.getBoundingClientRect();
-    let containerWidth = containerDimensions.width;
+    const containerWidth = item.offsetWidth;
 
-    nxtBtn[i].addEventListener('click', () => {
-        item.scrollLeft += containerWidth;
-    })
+    scnxtBtn[i].addEventListener('click', () => {
+        item.scrollBy({
+            left: containerWidth,
+            behavior: 'smooth'
+        });
+    });
 
-    preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-    })
-})
+    scpreBtn[i].addEventListener('click', () => {
+        item.scrollBy({
+            left: -containerWidth,
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Photography section
+const photoContainers = [...document.querySelectorAll('.photo-container')];
+const phnxtBtn = [...document.querySelectorAll('.phnxt-btn')];
+const phpreBtn = [...document.querySelectorAll('.phpre-btn')];
+
+photoContainers.forEach((item, i) => {
+    const containerWidth = item.offsetWidth;
+
+    phnxtBtn[i].addEventListener('click', () => {
+        item.scrollBy({
+            left: containerWidth,
+            behavior: 'smooth'
+        });
+    });
+
+    phpreBtn[i].addEventListener('click', () => {
+        item.scrollBy({
+            left: -containerWidth,
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Drawing section
+const drawContainers = [...document.querySelectorAll('.drawing-container')];
+const drnxtBtn = [...document.querySelectorAll('.drnxt-btn')];
+const drpreBtn = [...document.querySelectorAll('.drpre-btn')];
+
+drawContainers.forEach((item, i) => {
+    const containerWidth = item.offsetWidth;
+
+    drnxtBtn[i].addEventListener('click', () => {
+        item.scrollBy({
+            left: containerWidth,
+            behavior: 'smooth'
+        });
+    });
+
+    drpreBtn[i].addEventListener('click', () => {
+        item.scrollBy({
+            left: -containerWidth,
+            behavior: 'smooth'
+        });
+    });
+});
